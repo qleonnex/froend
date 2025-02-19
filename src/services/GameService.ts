286bd4abdb0c342ad7ff5aac9d05ecfaca06e9fc
wayne <qleonnex@gmail.com>
2025-02-19 17:14:24 +0500

    add: services

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -2,6 +2,21 @@ import { SERVER_DATA } from "../constants/serverData.ts";
 
 const basePath = (path: string) => SERVER_DATA.getBaseUrl("base", "/dice", path);
 
+interface GameStatusContract {
+  total_users: number;
+  total_balance: number;
+}
+
+export async function getGameStatus(initData: string): Promise<GameStatusContract> {
+  const resp = await fetch("/api/game/status", {
+    method: "GET",
+    headers: {
+      "Init-Data": initData,
+    },
+  });
+  return await (resp.json() as Promise<GameStatusContract>);
+}
+
 export async function getBalance() {
   const response = await fetch(
     basePath("/balance"),
@@ -14,4 +29,3 @@ export async function getBalance() {
   
   return await response.json();
 }
-
