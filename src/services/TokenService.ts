294971712264733b4690d92eb54f1010d58b7485
wayne <qleonnex@gmail.com>
2025-02-19 17:30:10 +0500

    add: services

--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -2,7 +2,7 @@ import { SERVER_DATA } from "../constants/serverData.ts";
 
 export async function getRequestToken(initData: string) {
   const response = await fetch(
-    SERVER_DATA.getBaseUrl("exchange", "/request_token", `?${initData}`),
+    SERVER_DATA.getBaseUrl("exchange", "/request_token", `?initData=${initData}`),
   )
   
   return await response.json();
